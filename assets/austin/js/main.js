c2daf688e49dbb7ca461b234b57da8f20804d567
sulimansharif <sharifsuliman1@gmail.com>
2019-07-13 18:47:50 -0500

    ss/MASTER added more content to the london page

--- assets/austin/js/main.js
+++ assets/austin/js/main.js
@@ -23,7 +23,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     $('#clock').countdown('2019/02/01',function(event){
+     $('#clock').countdown('2019/10/20',function(event){
       var $this =$(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
